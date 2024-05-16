0ca45e8c95b49070123c5a30b4538e5d3bd2a7c1
mathieu.pujol <mathieu.pujol@realfusio.com>
2024-05-16 10:04:17 +0200

    fix license

--- activityreportanalyzer.py
+++ activityreportanalyzer.py
@@ -1,10 +1,19 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-#
-# This program belongs to AKKODIS INGENIERIE PRODUIT SAS.
-# It is considered a trade secret, and is not to be divulged or used
-# by parties who have not received written authorization from the owner.
-#
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*- 
+# Copyright (C) converter3dji 2022 AKKODIS INGENIERIE PRODUIT SAS (support@realfusio.com)
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#         http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 
 ## DEPENDENCIES
 import json
