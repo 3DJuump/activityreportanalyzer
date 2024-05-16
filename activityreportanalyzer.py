9dfa032bf4945ef70c4e0977ba7d5e5b35f8d0d3
mathieu.pujol <mathieu.pujol@realfusio.com>
2024-05-16 10:05:14 +0200

    fix license

--- activityreportanalyzer.py
+++ activityreportanalyzer.py
@@ -1,6 +1,6 @@
-﻿#!/usr/bin/env python 
+#!/usr/bin/env python 
 # -*- coding: utf-8 -*- 
-# Copyright (C) converter3dji 2022 AKKA INGENIERIE PRODUIT (support@realfusio.com)
+# Copyright (C) converter3dji 2022 AKKODIS INGENIERIE PRODUIT SAS (support@realfusio.com)
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
